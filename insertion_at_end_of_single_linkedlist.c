1890d82c012350d6e01d597430e19d747343ab14
misbahmuneer <mizbabhat8@gmail.com>
2025-10-09 22:02:28 +0530

    insertion_at_end_of_single_linkedlist

---
+++ insertion_at_end_of_single_linkedlist.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* create_node(int n){
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=n;
+    newnode->next=NULL;
+    return newnode;
+    
+}
+struct node* createlist(int size){
+    struct node *head=NULL,*temp=NULL,*newnode=NULL;
+    int value;
+    for(int i=0;i<size;i++){
+      printf("enter the value for %d node",i+1);
+      scanf("%d",&value);
+      newnode=create_node(value);
+      if(head==NULL){
+        head=newnode;
+        temp=head;
+      }
+      else{
+        temp->next=newnode;
+        temp=newnode;
+      }
+    }
+    return head;
+}
+void display_linked_list(struct node* ptr){
+while(ptr!=NULL){
+    printf(" %d\n",ptr->data);
+    ptr=ptr->next;
+}
+}
+void add_node(struct node*ptr){
+    while(ptr->next!=NULL){
+        ptr=ptr->next;
+    }
+    int n;
+   
+    printf("enter the value you want to insert");
+     scanf("%d",&n);
+    struct node*newnode=create_node(n);
+    ptr->next=newnode;
+}
+void freeList(struct node* head) {
+    struct node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(){
+    int n;
+    printf("How many nodes do you want? ");
+    scanf("%d", &n);
+
+    struct node* head = createlist(n);
+    display_linked_list(head);
+    
+    add_node(head);
+     display_linked_list(head);
+    
+    freeList(head);
+
+    return 0;
+}
