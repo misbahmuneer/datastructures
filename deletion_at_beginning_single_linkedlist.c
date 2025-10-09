9010c708d125fb51d385833d20872db9123405db
misbahmuneer <mizbabhat8@gmail.com>
2025-10-09 22:10:11 +0530

    deletion_at_beginning_single_linkedlist

---
+++ deletion_at_beginning_single_linkedlist.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* create_node(int n){
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=n;
+    newnode->next=NULL;
+    return newnode;
+    
+}
+struct node* createlist(int size){
+    struct node *head=NULL,*temp=NULL,*newnode=NULL;
+    int value;
+    for(int i=0;i<size;i++){
+      printf("enter the value for %d node",i+1);
+      scanf("%d",&value);
+      newnode=create_node(value);
+      if(head==NULL){
+        head=newnode;
+        temp=head;
+      }
+      else{
+        temp->next=newnode;
+        temp=newnode;
+      }
+    }
+    return head;
+}
+void display_linked_list(struct node* ptr){
+while(ptr!=NULL){
+    printf(" %d\n",ptr->data);
+    ptr=ptr->next;
+}
+}
+struct node* delete_beg(struct node* head){
+    if (head == NULL) {
+        printf("List is already empty\n");
+        return NULL;
+    }
+
+    struct node* temp=head;
+    head=head->next;
+    free(temp);
+    return head;
+}
+
+void freeList(struct node* head) {
+    struct node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(){
+    int n;
+    printf("How many nodes do you want? ");
+    scanf("%d", &n);
+
+    struct node* head = createlist(n);
+    display_linked_list(head);
+    head=delete_beg(head);
+    display_linked_list(head);
+    freeList(head);
+   
+    return 0;
+}
