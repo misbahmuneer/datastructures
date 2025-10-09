b9b60dc92b94378a458037b49d1e15d867d28dbb
misbahmuneer <mizbabhat8@gmail.com>
2025-10-09 23:16:18 +0530

    insertion_at_beginning_double_linkedlist

---
+++ insertion_at_beginning_double_linkedlist.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node*next;
+    struct node* prev;
+};
+struct node* create_node(int n){
+     struct node* newnode=(struct node*)malloc(sizeof(struct node));
+     newnode->data=n;
+     newnode->next=NULL;
+     return newnode;
+}
+struct node* create_list(int n){
+    struct node* head=NULL,*temp=NULL,*newnode;
+    for(int i=0;i<n;i++){
+        int data;
+        printf("enter the value you want to enter in node");
+        scanf("%d",&data);
+        newnode=create_node(data);
+        if(head==NULL){
+            head=newnode;
+            head->prev=NULL;
+            temp=head;
+        }
+        else{
+            newnode->prev=temp;
+            temp->next=newnode;
+            temp=newnode;
+            temp->next=NULL;
+        }
+    }
+    return head;
+}
+void display(struct node* head){
+    struct node* temp=head;
+    while(temp!=NULL){
+        printf("%d\t",temp->data);
+        temp=temp->next;
+    }
+}
+void freeList(struct node* head) {
+    struct node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+struct node* insert_beg(struct node* head){
+    struct node * temp=head;
+    int n;
+    printf("enter the value you want to insert in new node");
+    scanf("%d",&n);
+    head=create_node(n);
+    head->prev=NULL;
+    head->next=temp;
+    temp->prev=head;
+    return head;
+}
+
+
+int main(){
+    int n;
+    printf("enter the number of nodes you want to insert");
+    scanf("%d",&n);
+    struct node* head=create_list(n);
+    display(head);
+    head=insert_beg(head);
+    display(head);
+    freeList(head);
+    return 0;
+}
